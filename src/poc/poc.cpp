4fcf1c9540f2d582df3083ceeea02b429bd2e54a
BHD Dev <dev@bhd.one>
2023-02-23 22:09:27 +0800

    Fix the average value of netspace.

--- src/poc/poc.cpp
+++ src/poc/poc.cpp
@@ -804,8 +804,9 @@ arith_uint256 CalculateAverageNetworkSpace(CBlockIndex *pindexCurr, Consensus::P
     int nActual{0};
     arith_uint256 result;
     while (nCount > 0 && pindex->nHeight >= params.BHDIP009Height) {
+        int nBitsOfFilter = pindex->nHeight < params.BHDIP009PlotIdBitsOfFilterEnableOnHeight ? 0 : params.BHDIP009PlotIdBitsOfFilter;
         auto netspace = chiapos::CalculateNetworkSpace(pindex->chiaposFields.nDifficulty, pindex->chiaposFields.GetTotalIters(),
-                params.BHDIP009DifficultyConstantFactorBits, params.BHDIP009PlotIdBitsOfFilter);
+                params.BHDIP009DifficultyConstantFactorBits, nBitsOfFilter);
         LogPrint(BCLog::POC, "%s: calculated netspace %s on height %ld\n", __func__, chiapos::FormatNumberStr(std::to_string(netspace.GetLow64())), pindex->nHeight);
         ++nActual;
         result += netspace;
