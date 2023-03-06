dc62f99d7994a43ace1b7c9b2a7282e5a74d4349
BHD Dev <dev@bhd.one>
2023-03-07 00:13:23 +0800

    Show vdf ips from the log of new best block.

--- src/validation.cpp
+++ src/validation.cpp
@@ -2863,12 +2863,14 @@ void static UpdateTip(const CBlockIndex* pindexNew, const CChainParams& chainPar
             AppendWarning(warningMessages, strprintf(_("%d of last 100 blocks have unexpected version").translated, nUpgraded));
     }
     ;
-    LogPrintf("%s: new best=%s height=%d version=0x%08x log2_work=%.8g tx=%lu date='%s' progress=%f cache=%.1fMiB(%utxo)%s, chia-work=%.5e\n", __func__,
-      pindexNew->GetBlockHash().ToString(), pindexNew->nHeight, pindexNew->nVersion,
-      log(pindexNew->nChainWork.getdouble())/log(2.0), (unsigned long)pindexNew->nChainTx,
-      FormatISO8601DateTime(pindexNew->GetBlockTime()),
-      GuessVerificationProgress(chainParams.TxData(), pindexNew), ::ChainstateActive().CoinsTip().DynamicMemoryUsage() * (1.0 / (1<<20)), ::ChainstateActive().CoinsTip().GetCacheSize(),
-      (!warningMessages.empty() ? strprintf(" warning='%s'", warningMessages) : ""), CalcChiaBlockWork(pindexNew->chiaposFields).getdouble());
+    LogPrintf("%s: new best=%s height=%d version=0x%08x log2_work=%.8g tx=%lu date='%s' progress=%f cache=%.1fMiB(%utxo)%s, chia-work=%.5e, vdf=%s ips\n", __func__,
+            pindexNew->GetBlockHash().ToString(), pindexNew->nHeight, pindexNew->nVersion,
+            log(pindexNew->nChainWork.getdouble())/log(2.0), (unsigned long)pindexNew->nChainTx,
+            FormatISO8601DateTime(pindexNew->GetBlockTime()),
+            GuessVerificationProgress(chainParams.TxData(), pindexNew), ::ChainstateActive().CoinsTip().DynamicMemoryUsage() * (1.0 / (1<<20)), ::ChainstateActive().CoinsTip().GetCacheSize(),
+            (!warningMessages.empty() ? strprintf(" warning='%s'", warningMessages) : ""), CalcChiaBlockWork(pindexNew->chiaposFields).getdouble(),
+            chiapos::FormatNumberStr(std::to_string(pindexNew->chiaposFields.GetTotalIters() / pindexNew->chiaposFields.GetTotalDuration()))
+    );
 
 }
 
