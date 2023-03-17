c64abefeb05c29cb5cb99c23c5f8c6de584cf3d2
BHD Dev <dev@bhd.one>
2023-03-17 19:25:01 +0800

    Update chia_test to test difficulty calculation.

--- src/chiapos/test/chia_test.cpp
+++ src/chiapos/test/chia_test.cpp
@@ -1,6 +1,7 @@
 #include <chainparams.h>
 
 #include <chiapos/post.h>
+#include <chiapos/miner/prover.h>
 #include <chiapos/kernel/calc_diff.h>
 #include <chiapos/kernel/utils.h>
 
@@ -17,6 +18,7 @@
 #include <cstdint>
 
 #include <gtest/gtest.h>
+#include <tinyformat.h>
 
 namespace chiapos {
 static char const* SZ_POOL_PK =
@@ -26,11 +28,6 @@ static char const* SZ_LOCAL_PK =
 static char const* SZ_FARMER_PK =
         "8b17c85e49be1a2303588b6fe9a0206dc0722c83db2281bb1aee695ae7e97c098672e1609a50b86786126cca3c9c8639";
 static char const* SZ_PLOT_ID = "7f88b755ddb5ee59c9a74b0c90a46b652ee8a3d9621f5b4500c5fb0a35ddbdd0";
-// static char const* SZ_PROOF =
-//         "cfc6e9bf214fd7ca3d45fed95d5b2d33e08027510c59d0c6089ba19d48ee5305ebadfbf1780e9e217fa179bb6a45671affaf0f37edcc2a"
-//         "ee43480f9bd6b86555c82924bfae14c53fae2d26f97f199699d99c5e323e85f650c8be84e531ff510d561e5ab55cbbfc77350ca986eba7"
-//         "2e25d555229ce4def80c6e5f06a1a8ecbd7a909f006addcfdb484a34dc7014b53b93ffc02cb4f15dfc83901335862a1c0d55ef87834378"
-//         "133120ed1d832dac071af1fecd3aebbdde306cd2729dbc30ed4aab1a591610d62364bd";
 static char const* SZ_CHALLENGE = "abd2fdbd2e6eece6171f3adcb4560acff92578ad33af3ebe2ad407b2101610ae";
 static const uint8_t K = 25;
 
@@ -119,66 +116,93 @@ TEST(Chiapos, Test_MakePlotId) {
 
 TEST(Chiapos, Test_VerifyChiaposProof) {
     uint256 challenge = uint256S("cc5ac4c68e9228f2487aa3d4a0ca067e150ad19f85934f5d97f4355c8c83fdbd");
-    Bytes vchProof = BytesFromHex("407f849c3b8fa9265751f34a72b57192cca83a5d7d7d2ce935cfde94e91ffa7567dadbe0cdd36e9da11c5ffd6b790b4acbe64a91d6e4c2f87b4e0b3f7d130222a3196fe705bbebf47817062f3deea06ea3c71dec4198ceaaa1f7fdad81e616c465bf4e8506a088ccd3ace16f1c0bdf9a9c73edcddc1cf0dcfacd8ef574809c442c9f8ffbd92defb3f520b27de1ae949201d63f618514af50994014f5a522bd5b67f6430fa927bda70c39b751c0a9a4a0a864889ed8202aecb283a708378002c5a6cf5f19fe05b31c");
-    PubKeyOrHash poolPkOrHash = MakePubKeyOrHash(PlotPubKeyType::OGPlots, BytesFromHex("92f7dbd5de62bfe6c752c957d7d17af1114500670819dfb149a055edaafcc77bd376b450d43eb1c3208a424b00abe950"));
-    PubKey localPk = MakeArray<PK_LEN>(BytesFromHex("b1578afd24055235e1a946108b84bab4c27b42f47e0a1f9562e251462b2f7564bd12991abcb9c23df5b62e77ed1f1ce7"));
-    PubKey farmerPk = MakeArray<PK_LEN>(BytesFromHex("8b17c85e49be1a2303588b6fe9a0206dc0722c83db2281bb1aee695ae7e97c098672e1609a50b86786126cca3c9c8639"));
+    Bytes vchProof = BytesFromHex(
+            "407f849c3b8fa9265751f34a72b57192cca83a5d7d7d2ce935cfde94e91ffa7567dadbe0cdd36e9da11c5ffd6b790b4acbe64a91d6"
+            "e4c2f87b4e0b3f7d130222a3196fe705bbebf47817062f3deea06ea3c71dec4198ceaaa1f7fdad81e616c465bf4e8506a088ccd3ac"
+            "e16f1c0bdf9a9c73edcddc1cf0dcfacd8ef574809c442c9f8ffbd92defb3f520b27de1ae949201d63f618514af50994014f5a522bd"
+            "5b67f6430fa927bda70c39b751c0a9a4a0a864889ed8202aecb283a708378002c5a6cf5f19fe05b31c");
+    PubKeyOrHash poolPkOrHash =
+            MakePubKeyOrHash(PlotPubKeyType::OGPlots, BytesFromHex("92f7dbd5de62bfe6c752c957d7d17af1114500670819dfb149a"
+                                                                   "055edaafcc77bd376b450d43eb1c3208a424b00abe950"));
+    PubKey localPk = MakeArray<PK_LEN>(BytesFromHex(
+            "b1578afd24055235e1a946108b84bab4c27b42f47e0a1f9562e251462b2f7564bd12991abcb9c23df5b62e77ed1f1ce7"));
+    PubKey farmerPk = MakeArray<PK_LEN>(BytesFromHex(
+            "8b17c85e49be1a2303588b6fe9a0206dc0722c83db2281bb1aee695ae7e97c098672e1609a50b86786126cca3c9c8639"));
     EXPECT_TRUE(VerifyPos(challenge, localPk, farmerPk, poolPkOrHash, K, vchProof, nullptr, 0));
 }
 
-TEST(utils, ParseHosts)
-{
-	static char const* SZ_HOSTS = "127.0.0.1:1991,sample.com:1676,none:1939,okthen:1919,noport.com";
-	auto entries = chiapos::ParseHostsStr(SZ_HOSTS, 19191);
-	EXPECT_EQ(entries.size(), 5);
-	EXPECT_EQ(entries[0].first, "127.0.0.1");
-	EXPECT_EQ(entries[0].second, 1991);
-	EXPECT_EQ(entries[1].first, "sample.com");
-	EXPECT_EQ(entries[1].second, 1676);
-	EXPECT_EQ(entries[2].first, "none");
-	EXPECT_EQ(entries[2].second, 1939);
-	EXPECT_EQ(entries[3].first, "okthen");
-	EXPECT_EQ(entries[3].second, 1919);
-	EXPECT_EQ(entries[4].first, "noport.com");
-	EXPECT_EQ(entries[4].second, 19191);
+TEST(utils, ParseHosts) {
+    static char const* SZ_HOSTS = "127.0.0.1:1991,sample.com:1676,none:1939,okthen:1919,noport.com";
+    auto entries = chiapos::ParseHostsStr(SZ_HOSTS, 19191);
+    EXPECT_EQ(entries.size(), 5);
+    EXPECT_EQ(entries[0].first, "127.0.0.1");
+    EXPECT_EQ(entries[0].second, 1991);
+    EXPECT_EQ(entries[1].first, "sample.com");
+    EXPECT_EQ(entries[1].second, 1676);
+    EXPECT_EQ(entries[2].first, "none");
+    EXPECT_EQ(entries[2].second, 1939);
+    EXPECT_EQ(entries[3].first, "okthen");
+    EXPECT_EQ(entries[3].second, 1919);
+    EXPECT_EQ(entries[4].first, "noport.com");
+    EXPECT_EQ(entries[4].second, 19191);
 }
-
-// Vdf proof verification
-// static char const* SZ_VDF_CHALLENGE = "9159bc7838880dcf826ba5fd7f5b693f203c01e29070ffa4eb1a73b727e09d84";
-// static const uint64_t VDF_ITERS = 100000;
-// static char const* SZ_VDF_Y =
-//         "0000e16a31edf6070934cacf78d3c3139e6986b7cebd0a45b996720fc916a163803a9c73d43b05c0835e8f2e4c52b2e10ae5623f7c1d5b"
-//         "98db"
-//         "2fc13b140b4c6035080f1b12cfe429abcee3912844319f2f81858d7ed4f7a6a108cf14f4a71090a1130201";
-// static char const* SZ_VDF_PROOF =
-//         "0200f845362c6a25e82034591c60797922723c20f7fc4fd2f955c43baefaa96ec4b3014174ff36ce05070f119a875b7b2df2ced3fe4963"
-//         "bc34"
-//         "fe7091e61623da6408117c6c31aef6d9b40f1e5c3c6fa7295196733d3448139a85bb8e6e5e0bc6dd020d07";
-//
-// TEST(Chiapos, Test_VerifyVdfProof) {
-//     CVdfProof proof;
-//     proof.challenge = uint256S(SZ_VDF_CHALLENGE);
-//     proof.vchY = BytesFromHex(SZ_VDF_Y);
-//     proof.vchProof = BytesFromHex(SZ_VDF_PROOF);
-//     proof.nWitnessType = 0;
-//     proof.nVdfIters = VDF_ITERS;
-//     proof.nVdfDuration = 0;
-//     CValidationState state;
-//     EXPECT_TRUE(CheckVdfProof(proof, state));
-// }
-
-// TEST(Difficulty, ExpectSize) {
-//     EXPECT_EQ(chiapos::calc::expected_plot_size<int64_t>(25), 855638016);
-//     EXPECT_EQ(chiapos::calc::expected_plot_size<arith_uint256>(25), 855638016);
-//     EXPECT_EQ(chiapos::calc::expected_plot_size<int64_t>(32), 139586437120);
-//     EXPECT_EQ(chiapos::calc::expected_plot_size<arith_uint256>(32), 139586437120);
-// }
 
 TEST(Utils, FormatNumberStr) {
     EXPECT_EQ(chiapos::FormatNumberStr("2022"), "2,022");
     EXPECT_EQ(chiapos::FormatNumberStr("202203"), "202,203");
     EXPECT_EQ(chiapos::FormatNumberStr("20220310"), "20,220,310");
     EXPECT_EQ(chiapos::FormatNumberStr("2022031010"), "2,022,031,010");
+}
+
+uint256 MakeRandUint256(uint8_t init_ch = 0) {
+    uint256 qs;
+    if (init_ch != 0) {
+        for (int i = 0; i < qs.size(); ++i) {
+            qs.begin()[i] = 0x55;
+        }
+    } else {
+        for (int i = 0; i < qs.size(); ++i) {
+            uint8_t val = rand() % 256;
+            qs.begin()[i] = val;
+        }
+    }
+    return qs;
+}
+
+TEST(Consensus, SteadyQualities) {
+    int const N = 5;
+    uint64_t diff = 150;
+    uint256 qs = MakeRandUint256(0x55);
+    uint64_t vdf_speed = 200000;
+    for (int i = 0; i < N; ++i) {
+        uint64_t iters = CalculateIterationsQuality(qs, diff, DIFFICULTY_CONSTANT_FACTOR_BITS, 32);
+        // adjust difficulty
+        uint64_t duration = std::max<uint64_t>(iters / vdf_speed, 1);
+        diff = AdjustDifficulty(diff, duration, 60 * 3);
+        LogPrintf("[%d] iters=%lld, duration=%d secs (%1.3f min), diff=%s\n", i, iters, duration,
+                  static_cast<double>(duration) / 60, MakeNumberStr(diff));
+    }
+}
+
+TEST(Consensus, RandomQualities) {
+    int const N = 300;
+    int const ADJUST_N = 10;
+    uint64_t diff{10000000};
+    srand(time(nullptr));
+    uint64_t vdf_speed = 200000;
+    for (int i = 0; i < N; ++i) {
+        uint256 qs = MakeRandUint256();
+        double q_in_plot;
+        arith_uint256 quality;
+        uint64_t iters =
+                CalculateIterationsQuality(qs, diff, DIFFICULTY_CONSTANT_FACTOR_BITS, 32, &q_in_plot, &quality);
+        // adjust difficulty
+        uint64_t duration = std::max<uint64_t>(iters / vdf_speed, 1);
+        diff = AdjustDifficulty(diff, duration, 60 * 3);
+        LogPrintf("[%d] iters=%lld, q=%1.3f, quality=%e duration=%d secs (%1.3f min), diff=%s%s\n", i, iters, q_in_plot,
+                  quality.getdouble(), duration, static_cast<double>(duration) / 60, MakeNumberStr(diff),
+                  (duration > 60 * 5 ? ", WARNING" : ""));
+    }
 }
 
 int RunAllTests() {
