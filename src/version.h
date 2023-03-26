a6367813b8345ba372acdc572effbf2ec4b17c74
BHD Dev <dev@bhd.one>
2023-03-26 11:54:26 +0800

    Update network version to 31.

--- src/version.h
+++ src/version.h
@@ -9,7 +9,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 80030;
+static const int PROTOCOL_VERSION = 80031;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
