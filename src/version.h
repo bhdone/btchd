7fa7ed68ed04327cf405dced1a86a037ba276246
BHD Dev <dev@bhd.one>
2023-03-31 01:40:57 +0800

    Update network version to 80032.

--- src/version.h
+++ src/version.h
@@ -9,7 +9,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 80031;
+static const int PROTOCOL_VERSION = 80032;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
