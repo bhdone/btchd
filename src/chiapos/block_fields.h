c9c79a1a6346fcb0ba95aa3428433a901c6577d0
BHD Dev <dev@bhd.one>
2023-04-02 00:03:34 +0800

    Update version.

--- src/chiapos/block_fields.h
+++ src/chiapos/block_fields.h
@@ -11,7 +11,7 @@
 
 namespace chiapos {
 
-const uint64_t CHIAHEADER_VERSION = 0x103;
+const uint64_t CHIAHEADER_VERSION = 0x104;
 
 class CPosProof {
 public:
--- src/version.h
+++ src/version.h
@@ -9,7 +9,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 80033;
+static const int PROTOCOL_VERSION = 80034;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
