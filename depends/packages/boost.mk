30f304d1bba0ed8edb9d13c2055482ac13763aeb
BHD Dev <dev@bhd.one>
2023-04-21 17:58:21 +0800

    Update to c++14 and fix warnings under clang.

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,8 +1,8 @@
 package=boost
-$(package)_version=1_79_0
-$(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/1.79.0/source
+$(package)_version=1_82_0
+$(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source
 $(package)_file_name=$(package)_$($(package)_version).tar.bz2
-$(package)_sha256_hash=475d589d51a7f8b3ba2ba4eda022b170e562ca3b760ee922c146b6c65856ef39
+$(package)_sha256_hash=a6e1ab9b0860e6a2881dd7b21fe9f737a095e5f33a3a874afc6a345228597ee6
 
 define $(package)_set_vars
 $(package)_config_opts_release=variant=release
