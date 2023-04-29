65d6527d3f153022762b4ce8ddb106332c3eca15
BHD Dev <dev@bhd.one>
2023-04-29 13:37:01 +0800

    Update fixed ip address to the seed list of testnet.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -471,7 +471,6 @@ public:
         vSeeds.clear();
         // nodes with support for servicebits filtering should be at the top
         vSeeds.push_back("testnet-seed0-chain.bhd.one");
-        vSeeds.push_back("timelord.bhd.one");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
