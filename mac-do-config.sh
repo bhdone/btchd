a6f4eca71db5a81b0c65fd5ce4e801321245363d
BHD Dev <dev@bhd.one>
2023-05-29 23:49:32 +0800

    Make compatible with current dir from `mac-do-config.sh`

--- mac-do-config.sh
+++ mac-do-config.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+CURDIR=`pwd`
+echo "config dir: ${CURDIR}"
+
 export PKG_CONFIG_PATH="/usr/local/opt/openssl@3/lib/pkgconfig"
-export BDB_PREFIX="$HOME/Workspace/BitcoinHD1/btchd/db4"
+export BDB_PREFIX="${CURDIR}/db4"
+
 ./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include"
