606899b9d9b2ca29ee1a8201800211f2a0137cce
BHD Dev <dev@bhd.one>
2023-05-30 19:12:02 +0800

    Fix `bhd_vdf.mk`.

--- depends/packages/bhd_vdf.mk
+++ depends/packages/bhd_vdf.mk
@@ -1,19 +1,16 @@
 package=bhd_vdf
-$(package)_version=v0.0.53
+$(package)_version=v0.0.54
 $(package)_download_path=https://github.com/bhdone/bhd_vdf/archive/refs/tags/
 $(package)_download_file=$($(package)_version).tar.gz
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=c19c26e9b7f09443851b7af0001ac5d84f6bc15d5998d58d8b1449e3949ec2c3
+$(package)_sha256_hash=10f28b59e334f9b3b2e7d6fa5ec447c52ed7fa446c6683eea6959dfce8ca1437
 
 define $(package)_fetch_cmds
 $(call fetch_file,$(package),$($(package)_download_path),$($(package)_download_file),$($(package)_file_name),$($(package)_sha256_hash))
 endef
 
-define $(package)_preprocess_cmds
-endef
-
 define $(package)_config_cmds
-  cmake -DCMAKE_TOOLCHAIN_FILE=$(BASEDIR)/hosts/cmake/$(host).cmake -DDOWNLOAD_EXTERNAL_MPIR=0 -DBUILD_TEST=0 -DCMAKE_INSTALL_PREFIX=$(host_prefix) -DFETCHCONTENT_BASE_DIR=$($(package)_source_dir) .
+  cmake -DCMAKE_TOOLCHAIN_FILE=$(BASEDIR)/hosts/cmake/$(host).cmake -DINCLUDE_GMP=1 -DBUILD_TEST=0 -DCMAKE_INSTALL_PREFIX=$(host_prefix) -DFETCHCONTENT_BASE_DIR=$($(package)_source_dir) .
 endef
 
 define $(package)_build_cmds
