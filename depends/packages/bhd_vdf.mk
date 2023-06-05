f321d8380b93b89e08c6d2edae230fcae9df1e72
BHD Dev <dev@bhd.one>
2023-06-05 19:45:21 +0800

    Use MPIR to fix the verify issue under Windows.

--- depends/packages/bhd_vdf.mk
+++ depends/packages/bhd_vdf.mk
@@ -1,16 +1,16 @@
 package=bhd_vdf
-$(package)_version=v0.0.55
+$(package)_version=v0.0.51
 $(package)_download_path=https://github.com/bhdone/bhd_vdf/archive/refs/tags/
 $(package)_download_file=$($(package)_version).tar.gz
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=515619a26b906da3ff61f40797e1c695bac745421cefda95decd5fc58c5acbd9
+$(package)_sha256_hash=4b13896f588924df08f95f8333a914d0e759933c75346bc32036c0427c99ae53
 
 define $(package)_fetch_cmds
 $(call fetch_file,$(package),$($(package)_download_path),$($(package)_download_file),$($(package)_file_name),$($(package)_sha256_hash))
 endef
 
 define $(package)_config_cmds
-  cmake -DCMAKE_TOOLCHAIN_FILE=$(BASEDIR)/hosts/cmake/$(host).cmake -DINCLUDE_GMP=1 -DBUILD_TEST=0 -DCMAKE_INSTALL_PREFIX=$(host_prefix) -DFETCHCONTENT_BASE_DIR=$($(package)_source_dir) .
+  cmake -DCMAKE_TOOLCHAIN_FILE=$(BASEDIR)/hosts/cmake/$(host).cmake -DBUILD_TEST=0 -DCMAKE_INSTALL_PREFIX=$(host_prefix) -DFETCHCONTENT_BASE_DIR=$($(package)_source_dir) .
 endef
 
 define $(package)_build_cmds
